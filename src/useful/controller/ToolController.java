72546bc73abcdae46e91a5a3fe7225648714ce68
Amber Bullough <amber@bertbullough.com>
2017-10-03 08:58:17 -0600

    Added data to Model package and added notes to Controller

--- src/useful/controller/ToolController.java
+++ src/useful/controller/ToolController.java
@@ -2,11 +2,22 @@ package useful.controller;
 
 import java.util.List;
 import java.util.ArrayList;
+import useful.model.Donut;
 
 public class ToolController 
 {
+	private List<Donut> donutList;
+	//Adds in the list
+	public ToolController()
+	{
+		donutList = new ArrayList<Donut>();
+	}
+	//Added in the array list.
 	public void start ()
 	{
+		Donut temp = new Donut();
 		
+		donutList.add(temp);
+		// Putting in the first lie of these two removed the error in the second line 
 	}
 }
