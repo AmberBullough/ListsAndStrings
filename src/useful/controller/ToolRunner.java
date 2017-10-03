7a901f90a5ace429b09a034e1e210f8387b52c6e
Amber Bullough <amber@bertbullough.com>
2017-10-03 08:33:14 -0600

    Initial Commit and gitIgnore

---
+++ src/useful/controller/ToolRunner.java
@@ -0,0 +1,16 @@
+package useful.controller;
+
+/**
+ * 
+ * @author abul1887
+ *
+ */
+
+public class ToolRunner 
+{
+	public static void main(String [] args)
+	{
+		ToolController myApp = new ToolController();
+		myApp.start();
+	}
+}
